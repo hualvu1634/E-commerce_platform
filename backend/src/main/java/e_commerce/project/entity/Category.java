c45d2092406cdc904d1f48fdc1a7417daacaf210
hualvu1634 <hualvu1325163@gmail.com>
2026-05-10 11:36:20 +0700

    update1005

--- backend/src/main/java/e_commerce/project/entity/Category.java
+++ backend/src/main/java/e_commerce/project/entity/Category.java
@@ -17,11 +17,12 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "categories") //danh muc
+@Table(name = "categories") 
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false,unique = true,length = 50)
     private String name;
+
 }
